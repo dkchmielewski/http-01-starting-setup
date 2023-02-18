3724a2f637273a979278b44f22a86a136595d7f0
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-02-18 17:17:34 -0500

    promisyfaing HTTP requests

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,36 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.open(method, url);
 
-xhr.responseType = 'json';
+    xhr.responseType = 'json';
 
-xhr.onload = function() {
-  // const listOfPosts = JSON.parse(xhr.response);
+    xhr.onload = function () {
+      // const listOfPosts = JSON.parse(xhr.response);
+    };
+
+    xhr.send();
+  });
+
+  return promise;
+}
+
+async function featchPosts() {
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
   const listOfPosts = xhr.response;
-  for(const post of listOfPosts) {
+  for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+featchPosts();
